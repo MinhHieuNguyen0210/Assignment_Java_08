7a05da0c3e6fd5fb6b2de20cf4a514c4f379ec12
minhhieu210 <43439957+minhhieu210@users.noreply.github.com>
2020-09-16 10:27:49 +0700

    CRUD Assignment Buoi 16

---
+++ SERVLET_CRM/src/com/java/DTO/UserDTO.java
@@ -0,0 +1,67 @@
+package com.java.DTO;
+
+public class UserDTO {
+	private int id;
+	private String email;
+	private String password;
+	private String fullName;
+	private String avatar;
+	private int role_id;
+	private String role_name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getAvatar() {
+		return avatar;
+	}
+
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+
+	public int getRole_id() {
+		return role_id;
+	}
+
+	public void setRole_id(int role_id) {
+		this.role_id = role_id;
+	}
+
+	public String getRole_name() {
+		return role_name;
+	}
+
+	public void setRole_name(String role_name) {
+		this.role_name = role_name;
+	}
+}
---
+++ SERVLET_CRM/src/com/java/controller/GroupWorkServlet.java
@@ -0,0 +1,43 @@
+package com.java.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = { "/jobs", "/jobs/add", "/jobs/details" })
+public class GroupWorkServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String action = req.getServletPath();
+
+		switch (action) {
+		case "/jobs":
+			req.getRequestDispatcher("/WEB-INF/views/groupwork/groupwork-table.jsp").forward(req, resp);
+
+			break;
+		case "/jobs/add":
+			req.getRequestDispatcher("/WEB-INF/views/groupwork/groupwork-add.jsp").forward(req, resp);
+
+			break;
+		case "/jobs/details":
+			req.getRequestDispatcher("/WEB-INF/views/groupwork/groupwork-details.jsp").forward(req, resp);
+			break;
+		default:
+			break;
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+}
